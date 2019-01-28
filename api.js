5e79414be14bf589dab55065b17c7ce26edf7f19
rdavies9 <44967478+rdavies9@users.noreply.github.com>
2019-01-28 20:52:42 +0000

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const apiRouter = express.Router();
-const employeesRouter = require('./employees.js');
-const menusRouter = require('./menus.js');
-
-apiRouter.use('/employees', employeesRouter);
-apiRouter.use('/menus', menusRouter);
-
-module.exports = apiRouter;
